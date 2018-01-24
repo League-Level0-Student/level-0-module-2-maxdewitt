5d47d61cf3db1273a38a2411d32f1b229059dee5
Dewitt.Max <Dewitt.Max@SEMS-259-09.SEMS259.LAB>
2018-01-24 14:56:13 -0800

    random

---
+++ src/random/LotteryNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class LotteryNumbers {
+public static void main(String[] args) {
+	
+	Random rand = new Random();
+	
+	int number1 = rand.nextInt(60 - 1) + 1
+	
+}
+	
+}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -6,12 +6,13 @@ package random;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class Validation {
 	public static void main(String[] args) {
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
+		for (int i = 0; i < 10; i++) {
 		int randomNumber = new Random().nextInt(5);
 		
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
@@ -19,7 +20,25 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		
+			
+		
+        if (randomNumber == 0) {
+        	JOptionPane.showMessageDialog(null, "You have nice hair!");
+        }
+        else if (randomNumber == 1) {
+        	JOptionPane.showMessageDialog(null, "Nice shoes!");	
+        	}
+        else if (randomNumber == 2) {
+        	JOptionPane.showMessageDialog(null, "Nice shirt!");	
+        	}
+        else if (randomNumber == 3) {
+        	JOptionPane.showMessageDialog(null, "Nice pants!");
+        	}
+        else {
+        	JOptionPane.showMessageDialog(null, "Have a nice day!");
+        }
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
